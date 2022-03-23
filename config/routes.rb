0a0761489378cc2345edcf2863855a82787ce529
mwataru-pro <m1130wata@gmail.com>
2022-03-23 16:21:14 +0900

    [add]CRUDの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'people/index'
+  get 'people', to: 'people#index'
+  get 'people/add'
+  post 'people/add', to: 'people#create'
+  get 'people/:id', to: 'people#show'
   get 'msgboard/index'
+  get 'people/edit/:id', to: 'people#edit'
+  patch 'people/edit/:id', to: 'people#update'
+  get 'people/delete/:id', to: 'people#delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'hello/index'
   get 'hello', to: 'hello#index'

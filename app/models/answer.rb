608e2a5675d7f3774995df8a2b78bdbfb3e04b7a
mwataru-pro <m1130wata@gmail.com>
2022-03-26 16:39:46 +0900

    [add]Q＆Aの追加

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,5 @@
 class Answer < ApplicationRecord
+  belongs_to : question
+
+  validates :content, :name, presence: {maessage: 'は、必須項目です。'}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_25_065244) do
+ActiveRecord::Schema.define(version: 2022_03_25_093011) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer "question_id"
+    t.text "content"
+    t.text "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "cards", force: :cascade do |t|
     t.text "title"
@@ -34,6 +42,15 @@ ActiveRecord::Schema.define(version: 2022_03_25_065244) do
     t.text "name"
     t.integer "age"
     t.text "mail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "title"
+    t.text "content"
+    t.text "name"
+    t.boolean "finished"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

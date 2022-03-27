ef6f34cc678eed4a0ca6dcd6301fa07ac2f257e3
mwataru-pro <m1130wata@gmail.com>
2022-03-27 15:03:20 +0900

    [update]アソシエーションの設定

--- app/models/board_message.rb
+++ app/models/board_message.rb
@@ -1,2 +1,5 @@
 class BoardMessage < ApplicationRecord
+  belongs_to :board_users
+
+  validates :content, presence: {message:'は、必須項目です。'}
 end
--- app/models/board_user.rb
+++ app/models/board_user.rb
@@ -1,2 +1,6 @@
 class BoardUser < ApplicationRecord
+  belongs_to :account
+  has_may :board_message
+
+  validates :nickname, presence: {message:'は、必須項目です。'}
 end
